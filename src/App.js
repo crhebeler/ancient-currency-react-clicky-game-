698052627dff662359e8e1e5a1bec843d08d5f7d
chrisheb1 <34869576+chrisheb1@users.noreply.github.com>
2018-06-29 21:53:16 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import Main from "./components/Main";
-import Header from "./components/Header";
+//import Header from "./components/Header";
 import Footer from "./components/Footer";
-import './App.css';
+//import Main from "./components/Main";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-    
-      </div>
-    );
-  }
-}
+const App = () => (
+ 
+    <div>
+
+
+    <Footer />
+    </div>
+
+);
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => (
+  <Main className="Main">
+    <span>Mainpage</span>
+  </Main>
+);
+
+export default Main;
