d6ddd49fd9747c2261c112501dac51bd25d7aa6b
Chris <chris@localhost.localdomain>
2018-07-01 13:07:55 -0400

    added a score card function

--- Ancient-currency-react/src/App.js
+++ Ancient-currency-react/src/App.js
@@ -3,9 +3,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 
+import ScoreCard from './components/ScoreCard';
+
+
 const App = () => (
  
     <div>
+
+<ScoreCard />
 <Header />
 <Main />
     <Footer />
