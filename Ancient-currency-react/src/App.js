34043542a66d8b0e17c315d7c4f99f66c45d50d7
Chris <chris@localhost.localdomain>
2018-07-01 12:41:33 -0400

    added HeaHeader

--- Ancient-currency-react/src/App.js
+++ Ancient-currency-react/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-//import Header from "./components/Header";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 
 const App = () => (
  
     <div>
-
+<Header />
 <Main />
     <Footer />
     </div>
---
+++ Ancient-currency-react/src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+
+const Header = () => (
+    <div className="header">
+    <h1>Ancient Currency</h1>
+    <h2>A Clicky Game Made With React.js</h2>
+    <p>Click on an image to earn points, but be careful, don't click on any more than once!</p>
+    </div>
+  );
+  
+
+
+
+export default Header;
+
