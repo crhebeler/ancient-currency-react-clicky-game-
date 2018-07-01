2fb88027c042440004f17659ef2e6349d211a1a1
Chris <chris@localhost.localdomain>
2018-07-01 12:23:00 -0400

    updated main compoent

---
+++ Ancient-currency-react/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <span> <p> &copy; Christopher Hebeler 2018 </p> </span>
+  </footer>
+);
+
+export default Footer;
